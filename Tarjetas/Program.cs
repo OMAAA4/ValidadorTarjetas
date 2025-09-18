b3eaa03857417fea71c002867347a1e37872d58a
Omar Acosta <omaaaacos@gmail.com>
2025-09-17 23:01:57 -0600

    Se agrega el metodo para ver el tipo de tarjeta si esta es valida

--- Tarjetas/Program.cs
+++ Tarjetas/Program.cs
@@ -4,13 +4,20 @@ namespace Tarjetas
 {
     internal class Program
     {
+        const int RestaSuperior = 9;
+        const int ComodinMultiProduct = 2;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            bool respuesta = ValidarNumero("16516516816849"); //AQUI EL NÚMERO
+            Console.WriteLine("\t..::VALIDAR::..");
+            string numeroTarjeta = "30569309025904"; //AQUI EL NÚMERO
+            string tipoTarjeta;
+            bool respuesta = ValidarNumero(numeroTarjeta); 
             if (respuesta)
-                Console.WriteLine("Tarjeta valida!!");
-            else Console.WriteLine("Tarjeta Invalida");
+            {
+                tipoTarjeta = TipoTarjeta(numeroTarjeta);
+                Console.WriteLine($"TARJETA VALIDA - TIPO: {tipoTarjeta}");
+            }
+            else Console.WriteLine("Número de tarjeta invalido");
         }
 
         static bool ValidarNumero(string num)
@@ -30,22 +37,26 @@ namespace Tarjetas
                     cadenaNumeros[iterador] = int.Parse(x.ToString());
                     iterador++;
                 }
+
                 cadenaProcesada = new int[cadenaNumeros.Length];
+
                 for (int i = 0; i < cadenaNumeros.Length; i++)
                 {
-                    bool par = i % 2!=0 ? true : false;
+                    bool par = i % ComodinMultiProduct != 0;
                     if (par)
-                        cadenaProcesada[i] = cadenaNumeros[i] * 2 > 9 ? (cadenaNumeros[i] * 2)-9 : cadenaNumeros[i] * 2;
+                        cadenaProcesada[i] = cadenaNumeros[i] * ComodinMultiProduct > RestaSuperior 
+                            ? (cadenaNumeros[i] * ComodinMultiProduct) - RestaSuperior 
+                            : cadenaNumeros[i] * ComodinMultiProduct;
                     else
                         cadenaProcesada[i] = cadenaNumeros[i];
                 }
 
                 foreach(int x in cadenaProcesada)
                 {
-                    suma = suma + x;
+                    suma += x;
                 }
 
-                if(suma%10 == 0)
+                if(suma % 10 == 0)
                 {
                     return true;
                 }
@@ -59,6 +70,67 @@ namespace Tarjetas
                 return false;
             }
             
+        }
+
+        static string TipoTarjeta(string num)
+        {
+            string prefijo = num[..8];
+            int largo = num.Length;
+
+            //Discover
+            if ((prefijo[..4] == "6011" || prefijo[..2] == "65" 
+                || (Convert.ToInt32(prefijo[..3]) >= 644 && Convert.ToInt32(prefijo[..3]) <= 649)
+                || (Convert.ToInt32(prefijo[..6]) >= 622126 && Convert.ToInt32(prefijo[..6]) <= 622925))
+                && (largo == 16 || largo == 19))
+            {
+                return "Discover";
+            }
+            //China UnionPay
+            else if (prefijo[..2] == "62" && (largo >= 16 && largo <= 19))
+            {
+                return "China UnionPay";
+            }
+            //Maestro Mastercard / antigua
+            else if ((prefijo[..2] == "50" 
+                || (Convert.ToInt32(prefijo[..2]) >= 56 && Convert.ToInt32(prefijo[..2]) <= 69))
+                && (largo >= 12 && largo <= 19))
+            {
+                return "MasterCard Maestro";
+            }
+            //Mastercard
+            else if (((Convert.ToInt32(prefijo[..2]) >= 51 && Convert.ToInt32(prefijo[..2]) <= 55) 
+                || (Convert.ToInt32(prefijo[..4]) >= 2221 && Convert.ToInt32(prefijo[..4]) <= 2720))
+                && largo == 16)
+            {
+                return "Mastercard";
+            }
+            //JCB
+            else if ((Convert.ToInt32(prefijo[..4]) >= 3528 && Convert.ToInt32(prefijo[..4]) <= 3589)
+                && largo == 16)
+            {
+                return "JCB";
+            }
+            //Diners Club
+            else if ((prefijo[..2] == "36" || prefijo[..2] == "38" 
+                || (Convert.ToInt32(prefijo[..3]) >= 300 && Convert.ToInt32(prefijo[..3]) <= 305))
+                && largo == 14)
+            {
+                return "Diners Club";
+            }
+            //American Express
+            else if ((prefijo[..2] == "34" || prefijo[..2] == "37") && largo == 15)
+            {
+                return "American Express";
+            }
+            //Visa
+            else if (prefijo[..1] == "4" && (largo == 13 || largo == 16 || largo ==19))
+            {
+                return "Visa";
+            }
+            else
+            {
+                return "Tipo No Registrada";
+            }
         }
     }
 }
